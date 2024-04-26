324d1046278ce36572d97ad662f04f6fa45a2408
David Costa <davidcostadev@gmail.com>
2024-04-26 16:47:09 -0300

    fix: add task validate before delete

--- src/graphql/mutations/task/task-destroy-mutation.ts
+++ src/graphql/mutations/task/task-destroy-mutation.ts
@@ -6,6 +6,16 @@ export const TaskDestroyMutation = mutationField('taskDestroy', {
     taskId: nonNull('UUID'),
   },
   resolve: async (parent, { taskId }, ctx) => {
+    const task = await ctx.prisma.task.findUnique({
+      where: {
+        id: taskId,
+      },
+    });
+
+    if (!task) {
+      throw new Error(`Task not found`);
+    }
+
     const data = await ctx.prisma.task.delete({
       where: {
         id: taskId,
--- src/graphql/queries/tasks.ts
+++ src/graphql/queries/tasks.ts
@@ -3,10 +3,7 @@ import { queryField } from 'nexus';
 export const Tasks = queryField('tasks', {
   type: 'TaskListResult',
   resolve: async (_root, _args, ctx) => {
-    console.log(ctx.prisma);
     const data = await ctx.prisma.task.findMany();
-
-    console.log(data);
 
     return {
       data,
