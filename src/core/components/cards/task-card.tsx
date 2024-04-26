60e0e908a3b3845052b98db9fc4b0ac3aaac6baa
David Costa <davidcostadev@gmail.com>
2024-04-26 17:30:54 -0300

    feat: add complete toggle button

--- src/core/components/cards/task-card.tsx
+++ src/core/components/cards/task-card.tsx
@@ -1,4 +1,5 @@
-import { Circle, Edit, Trash2 } from 'lucide-react';
+import clsx from 'clsx';
+import { Circle, CircleCheck, Edit, Trash2 } from 'lucide-react';
 import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 
@@ -6,6 +7,7 @@ import { TaskEditForm } from 'core/components/forms/task-edit-form';
 import { Button } from 'core/components/ui/button';
 import { useConfirm } from 'core/components/ui/confirm';
 import { useTaskDestroyMutation } from 'services/graphql/hooks';
+import { useTaskUpdateMutation } from 'services/graphql/hooks';
 import { TaskFragment } from 'services/graphql/types';
 
 type TaskCardProps = {
@@ -16,6 +18,7 @@ export const TaskCard = ({ task }: TaskCardProps) => {
   const [editFormVisibility, setEditFormVisibility] = useState(false);
   const { showConfirm } = useConfirm();
   const [taskDestroy] = useTaskDestroyMutation();
+  const [taskUpdate, { loading: isUpdating }] = useTaskUpdateMutation();
 
   const handleEditFormVisibility = () => setEditFormVisibility(!editFormVisibility);
 
@@ -47,14 +50,47 @@ export const TaskCard = ({ task }: TaskCardProps) => {
     setEditFormVisibility(false);
   };
 
+  const handleOnToggleComplete = async () => {
+    try {
+      const { data } = await taskUpdate({
+        variables: {
+          taskId: task.id,
+          input: {
+            isCompleted: !task.isCompleted,
+          },
+        },
+        refetchQueries: ['Tasks'],
+      });
+
+      if (data?.taskUpdate.data) {
+        toast.success('Task updated successfully');
+      } else {
+        throw new Error('Failed to update task');
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(`${error}`);
+    }
+  };
+
   if (editFormVisibility) {
     return <TaskEditForm onCancel={handleEditFormVisibility} onSave={handleOnSave} task={task} />;
   }
 
   return (
-    <div className="group flex gap-4 rounded-lg border-b border-slate-300 px-2 py-3 hover:bg-gray-50">
-      <button type="button" className="h-[36px] w-[36px] rounded-lg p-2 hover:bg-gray-100 active:bg-gray-100">
-        <Circle size={20} />
+    <div
+      className={clsx('group flex gap-4 rounded-lg border-b border-slate-300 px-2 py-3 ', {
+        'bg-gray-50': task.isCompleted,
+        'hover:bg-gray-50': !task.isCompleted,
+      })}
+    >
+      <button
+        type="button"
+        className="h-[36px] w-[36px] rounded-lg p-2 hover:bg-gray-100 active:bg-gray-100 disabled:bg-gray-100 disabled:opacity-80"
+        onClick={handleOnToggleComplete}
+        disabled={isUpdating}
+      >
+        {task.isCompleted ? <CircleCheck size={20} /> : <Circle size={20} />}
       </button>
       <div className="flex flex-1 flex-col gap-2">
         <h4 className="text-sm font-semibold">{task.title}</h4>
