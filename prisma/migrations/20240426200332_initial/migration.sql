7d43ea2eb6f8964391f988712f772e0728979cef
David Costa <davidcostadev@gmail.com>
2024-04-26 17:04:42 -0300

    feat: add prisma migration

---
+++ prisma/migrations/20240426200332_initial/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "is_completed" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "task_pkey" PRIMARY KEY ("id")
+);
