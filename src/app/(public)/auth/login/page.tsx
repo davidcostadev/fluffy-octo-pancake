016bc0af319163b316e5f2828986ad20e0d73047
David Costa <davidcostadev@gmail.com>
2024-04-27 14:41:09 -0300

    feat: add margin to login

--- src/app/(public)/auth/login/page.tsx
+++ src/app/(public)/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from 'core/components/forms/login-form';
 
 export default function LoginPage() {
   return (
-    <div className="mx-auto max-w-md rounded-xl border border-gray-300 bg-white p-5 drop-shadow-md">
+    <div className="mx-auto mt-14 max-w-md rounded-xl border border-gray-300 bg-white p-5 drop-shadow-md">
       <h1 className="py-2 text-center text-lg font-medium">Login</h1>
       <p className="mb-5 text-center text-sm text-gray-500">Log in to access the platform</p>
       <LoginForm />
