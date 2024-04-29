f5685e977b29add5c05d6fa6c80b8f78df65cd9d
David Costa <davidcostadev@gmail.com>
2024-04-28 21:12:37 -0300

    feat: logout layout wrapper

--- src/app/(private)/auth/layout.tsx
+++ src/app/(private)/auth/layout.tsx
@@ -1,19 +1,10 @@
-import { redirect } from 'next/navigation';
-
 import { AuthLayout as AuthLayoutWrapper } from 'core/components/layouts/auth-layout';
-import { getSession } from 'services/session/get-session';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const AuthLayout = async ({ children }: Props) => {
-  const session = await getSession();
-
-  if (session.token && session.user) {
-    redirect('/dashboard');
-  }
-
   return <AuthLayoutWrapper>{children}</AuthLayoutWrapper>;
 };
 
--- src/core/components/layouts/auth-layout.tsx
+++ src/core/components/layouts/auth-layout.tsx
@@ -1,10 +1,10 @@
 import { HeaderBrand } from 'core/components/ui/header-brand';
 
-type MainLayoutProps = {
+type AuthLayoutProps = {
   children: React.ReactNode;
 };
 
-export const MainLayout = ({ children }: MainLayoutProps) => {
+export const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <main className="mx-auto max-w-screen-md sm:px-5">
       <HeaderBrand isCentered={true} />
